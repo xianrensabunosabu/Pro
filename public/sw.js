bc0720935eaae4190863c7998beffd8725dc510b
xianrensabunosabu <xianrenchan@gmail.com>
2026-02-19 17:06:08 +0900

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,10 @@
+importScripts('uv.bundle.js');
+importScripts('uv.config.js');
+importScripts('uv.sw.js');
+
+const sw = new UVServiceWorker();
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(sw.fetch(event));
+});
+
